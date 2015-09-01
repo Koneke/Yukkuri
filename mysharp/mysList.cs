92626f0f699bf9bf6020fa60a777a4860ad36285
Lukas Hagman <lh.koneke@gmail.com>
2015-09-02 00:05:36 +0200

    start of evaluation state splitting

--- mysharp/mysList.cs
+++ mysharp/mysList.cs
@@ -34,6 +34,140 @@ namespace mysharp
 			throw new ArgumentException( "Symbol isn't defined." );
 		}
 
+		class EvaluationState
+		{
+			mysToken current;
+			int currentIndex;
+
+			Queue<mysToken> queue;
+			List<mysToken> currentExpression;
+			Stack<mysSymbolSpace> evaluationStack;
+
+			public void Create(
+				List<mysToken> initial,
+				Stack<mysSymbolSpace> spaceStack
+			) {
+				queue = new Queue<mysToken>();
+				currentExpression = new List<mysToken>( initial );
+
+				// why do we clone...? should reusing it be fine?
+				// even preferable, in fact?
+				evaluationStack = spaceStack.Clone();
+
+				currentIndex = currentExpression.Count;
+			}
+
+			// true if we should continue afterwards
+			void handleQuote() {
+				current.Quoted = false;
+				queue.Enqueue( current );
+				currentIndex--;
+			}
+
+			int evaluateFunctionGroup(
+				//Stack<mysSymbolSpace> evaluationStack,
+				out mysToken outval
+			) {
+				mysFunctionGroup fg = current as mysFunctionGroup;
+				List<mysToken> passedArgs = queue.Reverse().ToList();
+
+				while ( passedArgs.Count >= 0 ) {
+					mysFunction matching = fg.Judge( passedArgs );
+
+					if ( matching != null ) {
+						// call function and add our result back into
+						// the expression
+						mysToken returned;
+						if ( matching is mysBuiltin ) {
+							returned =
+								(matching as mysBuiltin).Call(
+								evaluationStack,
+								passedArgs
+							);
+						} else {
+							returned = matching.Call(
+								evaluationStack,
+								passedArgs
+							);
+						}
+
+						outval = returned;
+
+						return passedArgs.Count;
+					} else {
+						if ( passedArgs.Count > 0 ) {
+							// remove last, try again.
+							passedArgs.RemoveAt( passedArgs.Count - 1 );
+						} else {
+							throw new NoSuchSignatureException();
+						}
+					}
+				}
+
+				throw new NoSuchSignatureException();
+			}
+
+			void handleFunctionGroup() {
+				mysToken returned;
+
+				int taken = evaluateFunctionGroup(
+					out returned
+				);
+
+				currentExpression.RemoveRange(
+					currentIndex, taken + 1
+				);
+
+				if ( returned != null ) {
+					currentExpression.Insert(
+						currentIndex,
+						returned
+					);
+				}
+
+				currentIndex = currentExpression.Count;
+				queue.Clear();
+			}
+
+			public void Step() {
+				current = currentExpression.ElementAt( currentIndex );
+
+				if ( !current.Quoted ) {
+					// deref
+					while ( current.Type == mysTypes.Symbol ) {
+						current = EvaluateSymbol(
+							current as mysSymbol,
+							evaluationStack
+						);
+					}
+				} else {
+					handleQuote();
+					return;
+				}
+
+				switch ( current.Type ) {
+					case mysTypes.FunctionGroup:
+						handleFunctionGroup();
+						break;
+
+					case mysTypes.List:
+						mysToken returned = ( current as mysList )
+							.Evaluate( evaluationStack );
+
+						if ( returned != null ) {
+							queue.Enqueue( returned );
+						}
+						break;
+
+					default:
+						queue.Enqueue( current );
+						break;
+				}
+
+				currentIndex--;
+			}
+		}
+
 		int evaluateFunctionGroup(
 			mysFunctionGroup fg,
 			List<mysToken> passedArgs,
